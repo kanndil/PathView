cfaf3472150e80551db23a90861c44cea453e35b
Youssef Kandil <youssefkandil@aucegypt.edu>
2023-01-23 11:26:16 +0200

    rm approach one

--- approach_one/sta.tcl
+++
@@ -1,8 +0,0 @@
-set_cmd_units -time ns -capacitance pF -current mA -voltage V -resistance kOhm -distance um
-read_liberty sky130_fd_sc_hd.lib
-read_verilog i2c_master.v
-link_design i2c_master
-create_clock -name clk -period 20 sys_clk
-report_checks -path_delay min_max -format full -fields input_pins  > ./staReport.txt
-# Exit OpenSTA shell
-exit
